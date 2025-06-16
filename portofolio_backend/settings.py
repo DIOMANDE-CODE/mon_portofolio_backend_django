f0b775745ad14b48af40ae8e3859a9b241fc4259
DIOMANDE-CODE <diomandedroh79@gmail.com>
2025-06-16 22:30:07 +0000

    Configuration AWS pour stocker les fichiers medias

--- portofolio_backend/settings.py
+++ portofolio_backend/settings.py
@@ -166,23 +166,15 @@ EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", EMAIL_HOST_USER)
 
 
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
 AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_REGION_NAME = os.getenv('AWS_S3_REGION_NAME', 'us-east-2')
 
-# Pour éviter d’écraser un fichier déjà existant avec le même nom
 AWS_S3_FILE_OVERWRITE = False
-
-# Rendre les fichiers publics automatiquement
 AWS_DEFAULT_ACL = 'public-read'
 
-# URL personnalisée avec la bonne région
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com'
-
-# Pour générer les bonnes URLs des fichiers
 MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/'
-
-# Stockage principal pour les fichiers médias (images, etc.)
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-
